4b925583edf3a4ed68a084cc4634e3ef1272748c
Daniel Duque <duque@outlook.com>
2021-01-08 02:55:24 -0500

    Installed Enzyme, added basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders page title", () => {
+  const wrapper = shallow(<App />);
+  const title = <h1>My Good Reads</h1>;
+  expect(wrapper.contains(title)).toEqual(true);
 });
