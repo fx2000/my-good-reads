45e5565e93e0aa9b82b353b1d2142faa204e43ae
Daniel Duque <duque@outlook.com>
2021-01-08 09:23:07 -0500

    Improved layout

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -31,10 +31,12 @@ const Sidebar = ({ myBooks, setMyBooks }: IProps) => {
 
   return (
     <>
-      <h3 className="layout--sidebar--title">
-        {`My Reading Wishlist (${myBooks?.length})`}
-      </h3>
-      <div className="layout--sidebar--content" data-testid="sidebar-content">
+      <div className="layout--sidebar layout--sidebar--title" >
+        <h3>
+          {`My Reading Wishlist (${myBooks?.length})`}
+        </h3>
+      </div>
+      <div className="layout--sidebar layout--sidebar--content" data-testid="sidebar-content">
         {(myBooks?.length > 0) && myBooks?.map((book: Book, index: any) =>
           <div key={index} className="book--favorite">
             <p>{book.volumeInfo?.title}</p>
